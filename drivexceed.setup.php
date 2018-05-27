991962889b8532c5cb3c52217bc512a7a67358e3
Masga Satria Wirawan <39638126+masgasatriawirawan@users.noreply.github.com>
2018-05-27 17:03:57 +0700

    API key diubah

--- drivexceed.setup.php
+++ drivexceed.setup.php
@@ -8,7 +8,7 @@ if ( !isset( $_GET['id'] ) || !$_GET['id'] ) {
 	exit( json_encode( $log ) );
 }
 $id = $_GET['id'];
-$source = sprintf( 'https://www.googleapis.com/drive/v3/files/%s?alt=media&key=AIzaSyBXV3qGJ2rwDaxvUmAzaVpZMmn1t6PyU0E', $id );
+$source = sprintf( 'https://www.googleapis.com/drive/v3/files/%s?alt=media&key=AIzaSyD739-eb6NzS_KbVJq1K8ZAxnrMfkIqPyw', $id );
 $altsource = sprintf( 'https://zeydhan.me/drive/index.php?id=%s', $id );
 $image = sprintf( 'https://drive.google.com/thumbnail?id=%s&authuser=0&sz=w640-h360-n-k-rw', $id );
 ?>
